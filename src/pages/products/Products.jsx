d73055ff03c32dc89920b117cf58cc3ad57c2003
IVEWOR <howdy@deepslog.com>
2024-08-17 07:01:20 +0530

    products page design

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -13,13 +13,16 @@ export default function Products() {
     }, [])
     return (
         <Container>
-            <div className="grid grid-cols-4 gap-4">
+            <div className="py-8">
+                <h1 className="text-2xl text-center font-semibold">All Products</h1>
+            </div>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {products && products.map((item, index) => (
                     <div className="relative" key={index}>
-                        <div className="p-[20px] w-[100%] h-[350px] bg-zinc-100 flex items-center justify-center">
-                            <img className="mix-blend-multiply h-[200px] w-[auto]" src={item.image} alt={item.title} width="auto" height="200" />
+                        <div className="p-[20px] w-[100%] md:h-[350px] bg-zinc-100 flex items-center justify-center rounded-lg">
+                            <img className="mix-blend-multiply object-contain w-[100%] h-[170px] md:h-[260px] w-[auto]" src={item.image} alt={item.title} width="auto" height="200" />
                         </div>
-                        <h3 className="overflow-hidden truncate text-[15px] pt-2">{item.title}</h3>
+                        <h3 className="overflow-hidden truncate font-semibold text-[15px] pt-2">{item.title}</h3>
                         <div className="text-[15px]">${item.price}</div>
                         <a className="absolute top-0 left-0 right-0 bottom-0" href={`/products/${item.id}`}>
                             <span className="sr-only">
