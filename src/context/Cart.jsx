cc041e4f33063c7bce41ad1412a9940792d8d8a4
IVEWOR <howdy@deepslog.com>
2024-08-17 05:36:10 +0530

    items getting stored in the local storage

--- src/context/Cart.jsx
+++ src/context/Cart.jsx
@@ -1,9 +1,18 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext(null);
 
 export function CartProvider({ children }) {
-    const [items, setItems] = useState([]);
+    const [items, setItems] = useState(() => {
+        // get items from local
+        const savedItems = localStorage.getItem("cartItems");
+        return savedItems ? JSON.parse(savedItems) : [];
+    })
+
+    useEffect(() => {
+        // save items to local storage
+        localStorage.setItem("cartItems", JSON.stringify(items));
+    }, [items])
     return (
         <CartContext.Provider value={{ items, setItems }}>
             {children}
